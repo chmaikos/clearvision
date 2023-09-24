292ad3028a62e597b3d57439bcc9f09157318a6a
Chris Maikos <chmaikos@gmail.com>
2023-09-24 13:57:37 +0300

    [feat]: Pipeline now accepts custom weights

--- src/clearvision/__init__.py
+++ src/clearvision/__init__.py
@@ -1,25 +1,30 @@
 import logging
 import os
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+from logging.handlers import RotatingFileHandler
 
 # Suppress TensorFlow logging
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 logging.getLogger("tensorflow").setLevel(logging.FATAL)
 
-logger = logging.getLogger("clearvision")
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    handlers=[logging.FileHandler("clearvision.log"), logging.StreamHandler()],
-)
-logging.info("Success setting up")
+# Create and configure your custom logger
+logger = logging.getLogger("clearvision")
+logger.setLevel(logging.INFO)
+logger.propagate = False  # Prevents log messages from being passed to the root logger
+
+# Add handlers to your custom logger
+file_handler = RotatingFileHandler(
+    "clearvision.log", maxBytes=1e6, backupCount=3
+)  # 1 MB file size limit, keep last 3 logs
+stream_handler = logging.StreamHandler()
+
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 try:
     # Change here if project is renamed and does not equal the package name
--- src/clearvision/ocr.py
+++ src/clearvision/ocr.py
@@ -1,16 +1,32 @@
-import logging
 from typing import Any, Dict, Union
 
-import keras_ocr
+import image_ocr as keras_ocr
 import numpy as np
 
 from clearvision import logger
 
 
 class OCR:
-    def __init__(self, scale_factor: float = 2.0):
-        self.pipeline = keras_ocr.pipeline.Pipeline(scale=scale_factor)
-        logger.setLevel(logging.INFO)
+    def __init__(
+        self,
+        scale_factor: float = 2.0,
+        recognizer_weights: str = None,
+        detector_weights: str = None,
+    ):
+        self.detector = (
+            keras_ocr.detection.Detector(weights_path=detector_weights)
+            if detector_weights
+            else keras_ocr.detection.Detector()
+        )
+        self.recognizer = (
+            keras_ocr.recognition.Recognizer(weights_path=recognizer_weights)
+            if recognizer_weights
+            else keras_ocr.recognition.Recognizer()
+        )
+        self.pipeline = keras_ocr.pipeline.Pipeline(
+            detector=self.detector, recognizer=self.recognizer, scale=scale_factor
+        )
+        logger.info("Pipeline setup complete")
 
     def perform_ocr(self, image_input: Union[str, np.ndarray]) -> Union[Dict, None]:
         img = self._read_image(image_input)
